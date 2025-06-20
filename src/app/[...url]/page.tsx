22a27111e7dc32a25b872d043a1abff4df497f0d
Dru-429 <ect.dhruv@gmail.com>
2025-06-20 11:22:21 +0530

    Added Redis DB

--- src/app/[...url]/page.tsx
+++ src/app/[...url]/page.tsx
@@ -1,4 +1,5 @@
 import { ragChat } from '@/lib/rag-chat'
+import { redis } from '@/lib/redis'
 import React from 'react'
 
 interface PageProps {
@@ -15,18 +16,29 @@ function reconstructURL({url} : {url:string[]}){
 }
  
 const page = async ({ params }: PageProps ) => {
-    console.log('params', params)
     
     const reUrl = reconstructURL({ url: params.url as string[]})
-    await ragChat.context.add({
-        type: "html",
-        source: reUrl,
-        config: { chunkOverlap: 50, chunkSize: 1000 }
-    })
+
+    const isIndexed = await redis.sismember("indexed-urls", reUrl)  //sismember checks if the URL is already indexed in Redis Set (name of set, to check)
+
+    if (!isIndexed) {
+        await ragChat.context.add({
+            type: "html",
+            source: reUrl,
+            config: { chunkOverlap: 50, chunkSize: 1000 }
+        })
+
+        await redis.sadd("indexed-urls", reUrl) //sadd adds the URL to the Redis Set
+    }
+    
 
   return (
     <div>
         hi there
+        <iframe
+            src={reUrl}
+            style={{ width: '80%', height: '100vh', border: 'none' }}
+        />
     </div>
   )
 }
